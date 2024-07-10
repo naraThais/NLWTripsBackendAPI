ffc4dc431f0df7d93d9f94891fb4392556c7070d
naraThais <thaisna46@gmail.com>
2024-07-09 21:24:53 -0300

    Criando e add biblioteca para envio de e-mail com nodemailer e criando validação com shcema pela lib ZOD

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod' //schama validation
+import nodemailer from 'nodemailer'
+import dayjs, { Dayjs } from "dayjs";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
 
 
 export async function createTrip(app: FastifyInstance){
@@ -13,16 +17,48 @@ export async function createTrip(app: FastifyInstance){
                  apenas tipos primitivos, então a data vai chegar como String '2024-06-23 10:00:00'*/
                 starts_at: z.coerce.date(),
                 ends_at: z.coerce.date(),
+                owner_name: z.string(),
+                owner_email: z.string().email(),
+
                 
             })
         },
     }, async (request) => {
-        const { destination, starts_at, ends_at} = request.body
+        const { destination, starts_at, ends_at, owner_name, owner_email} = request.body
         
-        return {
-            destination,
-            starts_at,
-            ends_at
+        if (dayjs(starts_at).isBefore(new Date())){
+            throw new Error('Invalid trip start date.')
         }
+
+        if (dayjs(ends_at).isBefore(starts_at)){
+            throw new Error('Invalid trip day, the ends is before the start.')
+        }
+
+        const trip = await prisma.trip.create({
+            data:{
+                destination,
+                starts_at,
+                ends_at,
+            }
+        })
+
+        const mail = await getMailClient()
+
+       const message = await mail.sendMail({
+            from:{
+                name: 'Equipe Plann.er',
+                address: 'oi@plann.er',
+            },
+            to:{
+                name: owner_name,
+                address: owner_email,
+            },
+            subject: 'Testand envio de E-mail.',
+            html: `<p> Teste do envio de E-mail </p>`,
+        })
+
+        console.log(nodemailer.getTestMessageUrl(message))
+
+        return { tripId: trip.id }
     })
 }
